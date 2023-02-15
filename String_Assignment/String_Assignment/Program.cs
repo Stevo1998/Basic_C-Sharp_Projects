194ff9cd8e22142c8d42df6d56794ba4ddbc9f22
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-15 15:07:45 -0800

    Added a folder containing a C# console app

---
+++ String_Assignment/String_Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello,".ToUpper(); // Creating a string variable
+            string name = "My name is Leonardo."; // Creating a string variable
+            string comment = "Nice to meet you!"; // Creating a string variable
+
+            StringBuilder sb = new StringBuilder(); // Creating a StringBuilder
+            sb.Append(greeting + "\n" + name + "\n" + comment); // Appeding the created string variables to the stringbuilder
+
+            Console.WriteLine(sb); // Displaying the string created on the StringBuilder variable to the console
+            Console.Read(); // Pauses the application until user input.
+
+        }
+    }
+}
