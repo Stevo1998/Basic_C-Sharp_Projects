b3f4040220879165b267c86e97e4d239ef37dd38
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-15 12:54:56 -0800

    Added a folder containing a C# console app

---
+++ Console_App_WhileLoop_ Assignment/Console_App_WhileLoop_ Assignment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_WhileLoop__Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is how you count from zero to five in Spanish:");
+            int numero = 0;
+
+            do
+            {
+                Console.WriteLine("Cero");
+                numero++;
+
+                Console.WriteLine("Uno");
+                numero++;
+
+                Console.WriteLine("Dos");
+                numero++;
+
+                Console.WriteLine("Tres");
+                numero++;
+
+                Console.WriteLine("Cuatro");
+                numero++;
+
+                Console.WriteLine("Cinco");
+                numero++;
+            }
+            while (numero < 6);
+
+            Console.WriteLine("\nThis how how you count from zero to five in English:");
+            int number = 0;
+
+            while (number < 6)
+            {
+                Console.WriteLine("Zero");
+                number++;
+
+                Console.WriteLine("One");
+                number++;
+
+                Console.WriteLine("Two");
+                number++;
+
+                Console.WriteLine("Three");
+                number++;
+
+                Console.WriteLine("Four");
+                number++;
+
+                Console.WriteLine("Five");
+                number++;
+            }
+
+            Console.Read();
+        }
+    }
+}
