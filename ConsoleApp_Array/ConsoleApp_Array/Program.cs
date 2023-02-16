7cab6d9517ec7266be817100ed5994b5597ff4cf
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-15 17:28:44 -0800

    Fixed the negative numbers bug

--- ConsoleApp_Array/ConsoleApp_Array/Program.cs
+++ ConsoleApp_Array/ConsoleApp_Array/Program.cs
@@ -10,26 +10,26 @@ internal class Program
         Console.WriteLine("Enter a number from 0 to 4 to display a message."); // Asking user for input
         int message = Convert.ToInt32(Console.ReadLine()); // Converting user input to integer 
 
-        if (message <= 4) // if user input is less than or queal to 4, then...
-        {
-            Console.WriteLine("Message: " + stringArray[message]); // Display the string in the array at the index of user input
-        }
-        else // if user input is more than 4, then...
+        if (message < 0 || message > stringArray.Length - 1) // If user input is less than 0 more than the length of the array, then...
         {
             Console.WriteLine("The number entered was not from 0 to 4."); // Display the string on the console
+        }
+        else // if user input meets a number in the array index, then...
+        {
+            Console.WriteLine("Message: " + stringArray[message]); // Display the string in the array at the index of user input
         }
 
         int[] numArray = { 0, 1, 4, 9, 16 }; // Creating an integer array
         Console.WriteLine("\nEnter a number from 0 to 4 to display a square number."); // Asking user for input
         int number = Convert.ToInt32(Console.ReadLine()); // Converting user input into an integer
 
-        if (number <= 4) // If user input is less than or queal to 4, then...
-        {
-            Console.WriteLine("Square number: " + numArray[number]); // Display the interger in the array at the index of user input
-        }
-        else // if user input is more than 4, then..
+        if (number < 0 || number > numArray.Length - 1 ) // If user input is less than 0 more than the length of the array, then...
         {
             Console.WriteLine("The number entered was not from 0 to 4."); // Display string on the console
+        }
+        else // if user input meets an index number in the array, then...
+        {
+            Console.WriteLine("Square number: " + numArray[number]); // Display the interger in the array at the index of user input
         }
 
         List<string> stringList = new List<string>(); // Creating a list of strings
@@ -39,13 +39,14 @@ internal class Program
         Console.WriteLine("\nEnter a number from 0 to 2 to display a question"); // Asking user for input
         int question = Convert.ToInt32(Console.ReadLine()); // Converting user input into an integer
 
-        if (question <= 2) // If user input is less than or equal to 2, then...
-        {
-            Console.WriteLine("Question: " + stringList[question]); // Display the string in the list at the index of user input
-        }
-        else // If user input is more than 2, then...
+        if (question < 0 || question > stringList.Count - 1) // If user input is less than 0 or more than the lenth of the list, then...
         {
             Console.WriteLine("The number entered was not from 0 to 2"); // Display string to the console
+            
+        }
+        else // If user input meets an index number in the list, then... 
+        {
+            Console.WriteLine("Question: " + stringList[question]); // Display the string in the list at the index of user input
         }
         Console.Read(); // Pausing the apllication until user input
     }
