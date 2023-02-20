5438cab4a2ad54919103c507343cf5eb98db199d
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-20 11:44:03 -0800

    Added a folder containing a C# console application

---
+++ Calling_Methods/Calling_Methods/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class Math
+        {
+            public int integer { get; set; } // Creating a integer property
+
+            public static int addMethod(int integer) // Creating a method in which 5 is added to integer and returns the result
+        {
+                return integer + 5;
+            }
+
+            public static int multiplyMethod(int integer) // Creating a medthod in which integer is multiplied by 5 and returns the result
+        {
+                return integer * 5;
+            }
+
+            public static int DivideMethod(int integer) // Creating a method in which integer is divided by 2 and returns the result
+            {
+                return integer / 2;
+            }
+        }
+}
---
+++ Calling_Methods/Calling_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number:"); // Asking user for input 
+            Math userNum = new Math(); // Crating an object of the class Math
+            userNum.integer = Convert.ToInt32(Console.ReadLine()); // Assigning user input to the object of the class Math
+
+
+            int resultAdd = Math.addMethod(userNum.integer); // Calling the addMethod from the Math Class
+            int resultMultiply = Math.multiplyMethod(userNum.integer); // Calling the multiplyMethod from the Math class
+            int resultDivide = Math.DivideMethod(userNum.integer); // Calling the divideMethod from the math class
+
+
+            Console.WriteLine // Displaying math results from each method to the user
+                ( 
+                    "Your number plus 5 = " + resultAdd.ToString() +
+                    "\nYour number multiplied by 5 = " + resultMultiply.ToString() + 
+                    "\nYour number divided by 2 = " + resultDivide.ToString()
+                );
+
+            Console.Read(); // Pausing the app until user input
+        }
+    }
+}
