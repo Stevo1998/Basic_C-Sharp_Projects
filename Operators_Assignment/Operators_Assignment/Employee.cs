2031a9ac65026eeae68a48e328db715bcd5457c7
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-21 17:16:16 -0800

    Added a folder containing a C# console app

---
+++ Operators_Assignment/Operators_Assignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee // Defining a public class called Employee
+    {
+        public int Id { get; set; } // Defining an integer property
+        public string firstName { get; set; } // Defining a string property
+        public string lastName { get; set; } // Defining a string property
+
+        public static bool operator ==(Employee employee1, Employee employee2) // Overloading the == operator
+        {
+            if (employee1.Id == employee2.Id) // If the id properties of the objects are the same return true else return false
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2) // Overloading the != operator
+        {
+            if (employee1.Id != employee2.Id) // If the id properties of the objects are not the same return true else return false
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Operators_Assignment/Operators_Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); // Instatiating a new object of Employee type as employee1
+            employee1.Id = 0; // Creating an Id object of employee1
+            employee1.firstName = "Sample1"; // Creating a object of employee1
+            employee1.lastName = "Employee"; // Creating a object of employee1
+
+            Employee employee2 = new Employee(); // Instatiating a new object of Employee type as employee2
+            employee2.Id = 1; // Creating an Id object of employee2
+            employee2.firstName = "Sample2"; // Creating a firstName object of employee2
+            employee2.lastName = "Sample2"; // Creating a lastName object of employee2
+
+
+            Console.WriteLine(employee1 == employee2); // Displaying result of overloaded operator to the console
+            Console.WriteLine(employee1 != employee2); // Displaying result of overloaded operator to the console
+
+            Console.Read();
+        }
+    }
+}
