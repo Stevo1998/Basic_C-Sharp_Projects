c3af0374c078e85302dc11cf7dd5099e8f17463e
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-02-23 13:42:37 -0800

    Added a folder containing a C# console app

---
+++ Input_Assignment/Input_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            string userInput = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Steven Salazar\OneDrive\Escritorio\Basic_C#_Programs\Logs\log2.txt", userInput);
+
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Steven Salazar\OneDrive\Escritorio\Basic_C#_Programs\Logs\log2.txt"));
+
+            Console.Read();
+        }
+    }
+}
