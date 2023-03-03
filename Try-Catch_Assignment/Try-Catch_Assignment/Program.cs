b431992f659f13f5534d94010abdfe756c763bf6
Leonardo S Salazar <steven.salazar583@gmail.com>
2023-03-03 10:54:45 -0800

    Added a new folder
    Added a new folder

---
+++ Try-Catch_Assignment/Try-Catch_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                DateTime currentYear = DateTime.Today;
+                int birthYear = currentYear.Year - age;
+                Console.WriteLine("Your birth year is {0}", birthYear.ToString());
+                Console.Read();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Please enter a whole number.");
+            }
+            finally { Console.Read(); }
+        }
+    }
+}
